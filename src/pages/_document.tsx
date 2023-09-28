d62443a4c5398d10a950330399c7fe125d8d4de3
Yurii Liubuskyi <liubuskiy.yuri@gmail.com>
2023-09-28 19:38:41 +0300

    minor fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,10 @@
-import { Head, Html, Main, NextScript } from 'next/document';
+import React from 'react';
+import type { DocumentContext } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
-export default function Document() {
+import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs';
+
+function MyDocument() {
   return (
     <Html lang="en">
       <Head />
@@ -11,3 +15,31 @@ export default function Document() {
     </Html>
   );
 }
+
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const cache = createCache();
+  const originalRenderPage = ctx.renderPage;
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: App => props => (
+        <StyleProvider cache={cache}>
+          <App {...props} />
+        </StyleProvider>
+      )
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+  const style = extractStyle(cache, true);
+  return {
+    ...initialProps,
+    styles: (
+      <>
+        {initialProps.styles}
+        {/* eslint-disable-next-line react/no-danger */}
+        <style dangerouslySetInnerHTML={{ __html: style }} />
+      </>
+    )
+  };
+};
+
+export default MyDocument;
