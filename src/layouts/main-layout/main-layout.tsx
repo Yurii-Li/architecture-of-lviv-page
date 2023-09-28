28cefb66db40d4575c1fb4b08fdffac1030711f8
Yurii Liubuskyi <liubuskiy.yuri@gmail.com>
2023-09-28 19:31:55 +0300

    tasks/arch-lv-1-header: header

---
+++ src/layouts/main-layout/main-layout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react';
+import { Inter } from 'next/font/google';
+
+import Header from '@/components/header/header';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function MainLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className={inter.className}>
+      <Header />
+      {children}
+    </div>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+
+import MainLayout from '@/layouts/main-layout/main-layout';
+
+import '@/styles/globals.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MainLayout>
+      <Component {...pageProps} />
+    </MainLayout>
+  );
 }
