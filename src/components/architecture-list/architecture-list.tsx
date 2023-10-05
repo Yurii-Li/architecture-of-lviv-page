e4f16dfd4ab63d9edc907ac8b590f74ba78ae4ed
Yurii Liubuskyi <liubuskiy.yuri@gmail.com>
2023-10-05 19:25:57 +0300

    tasks/arch-lv-4-page-of-the-architectural-landmark: route to landmark

--- src/components/architecture-list/architecture-list.tsx
+++ src/components/architecture-list/architecture-list.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 import ArchitectureCard from '@/components/architecture-card/architecture-card';
 import { IArchitectureCard } from '@/interfaces/architecture.interface';
 
@@ -6,10 +9,19 @@ interface IListProps {
 }
 
 export default function ArchitectureList({ list }: IListProps) {
+  const { query } = useRouter();
+
   return (
     <div className="architecture-list">
       {list.map(item => (
-        <ArchitectureCard architecture={item} key={item.id} />
+        <Link
+          key={item.id}
+          href={{
+            pathname: `/architecture/${query.category}/${item.id}`
+          }}
+        >
+          <ArchitectureCard architecture={item} />
+        </Link>
       ))}
     </div>
   );
---
+++ src/pages/architecture/[category]/[id]/index.tsx
@@ -0,0 +1,3 @@
+export default function LandmarkPage() {
+  return <div>LandmarkPage</div>;
+}
