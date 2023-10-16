82c0d46cdf8778ab29746183da3b100880272e14
Yuri Liubuskyi <liubuskiy.yuri@gmail.com>
2023-10-16 18:37:46 +0300

    tasks/arch-lv-5-footer: add footer

--- src/layouts/main-layout/main-layout.tsx
+++ src/layouts/main-layout/main-layout.tsx
@@ -1,15 +1,19 @@
 import React, { ReactNode } from 'react';
 import { Inter } from 'next/font/google';
 
+import Footer from '@/components/footer/footer';
 import Header from '@/components/header/header';
+
+import styles from './main-layout.module.scss';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function MainLayout({ children }: { children: ReactNode }) {
   return (
-    <div className={inter.className}>
+    <div className={`${inter.className} ${styles.mainLayout}`}>
       <Header />
-      <main>{children}</main>
+      <main className={styles.main}>{children}</main>
+      <Footer />
     </div>
   );
 }
