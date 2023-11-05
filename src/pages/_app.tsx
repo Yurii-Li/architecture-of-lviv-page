6cdc94d3700482d1ca0370a8fbb70665dbd90eec
Yuri Liubuskyi <liubuskiy.yuri@gmail.com>
2023-11-05 16:55:43 +0200

    tasks/arch-lv-10-custom-theme: font family for ant design

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import type { AppProps } from 'next/app';
+import { ConfigProvider } from 'antd';
 
 import MainLayout from '@/layouts/main-layout/main-layout';
+import customTheme from '@/theme/custom-theme';
 
 import '@/styles/globals.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
+    <ConfigProvider theme={customTheme}>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </ConfigProvider>
   );
 }
