09562708c90160e78185b7e20d7aee8b3e2b93bc
Yuri Liubuskyi <liubuskiy.yuri@gmail.com>
2023-11-06 15:23:35 +0200

    fix/arch-lv-3-nav-menu-styles: bg menu transparent and hover item color #1677FF

--- src/theme/custom-theme.ts
+++ src/theme/custom-theme.ts
@@ -6,6 +6,12 @@ const inter = Inter({ subsets: ['latin'] });
 const customTheme: ThemeConfig = {
   token: {
     fontFamily: inter.toString()
+  },
+  components: {
+    Menu: {
+      itemBg: 'transparent',
+      itemHoverColor: '#1677FF'
+    }
   }
 };
 
