245b2871963020fc96436c679df4508d8069c849
Yuri Liubuskyi <liubuskiy.yuri@gmail.com>
2023-11-07 14:30:10 +0200

    tasks/arch-lv-1-header: logo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,8 +1,12 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 import NavMenu from '@/components/nav-menu/nav-menu';
 import Search from '@/components/search/search';
+
+import logoBlack from '../../../public/logo-black.svg';
+import logoWhite from '../../../public/logo-white.svg';
 
 import styles from './header.module.scss';
 
@@ -11,10 +15,14 @@ export default function Header() {
 
   const notShowNavMenu = pathname === '/architecture/[category]/[id]';
 
+  const logo = pathname === '/' ? logoWhite : logoBlack;
+
   return (
     <header className={styles.header}>
       <div className={styles.top}>
-        <Link href="/">Logo</Link>
+        <Link href="/">
+          <Image src={logo} alt="logo" width={150} height={30} />
+        </Link>
         <Search />
       </div>
       {!notShowNavMenu && <NavMenu />}
